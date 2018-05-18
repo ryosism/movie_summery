632591e8976e3417ea4bea8f2a8da77ea36f3cf9
ryosism <sn0wbreeze2000@yahoo.co.jp>
2018-05-18 17:55:33 +0900

    work correctly

--- summery.py
+++ summery.py
@@ -1,9 +1,8 @@
 import sys
-import Tkinter
+import tkinter
 
-
-root = Tkinter.Tk()
-root.title(u"Software Title")
+root = tkinter.Tk()
+root.title("summery movie!")
 root.geometry("400x300")
 
 root.mainloop()
