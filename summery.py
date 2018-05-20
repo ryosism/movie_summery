2af45d18c8fd6ae12f3d3fde0ff4cbaf0407c317
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2018-05-20 19:20:56 +0900

    [追加]top5が入る画像の枠も作ったけど、動作未確認、実際の配列が欲しい

--- summery.py
+++ summery.py
@@ -10,16 +10,24 @@ class Frame(Tk.Frame):
         self.master.title("summery movie!")
         self.master.geometry("1200x800")
 
+        #クエリ画像表示
         self.queryImg = []
-        for i, file in enumerate(filepath):
+        for row, file in enumerate(filepath):
             image = PIL.Image.open(file)
             self.queryImg.append(PIL.ImageTk.PhotoImage(image))
-
-            code = "self.query{} = Tk.Label(self, image=self.queryImg[{}])".format(i, i)
+            code = "self.query{} = Tk.Label(self, image=self.queryImg[{}])".format(row, row)
+            exec(code)
+            code = "self.query{}.grid(row={}, column=0, padx = 10, pady = 10)".format(row, row)
             exec(code)
 
-            code = "self.query{}.pack(padx = 10, pady = 10, anchor = Tk.NW)".format(i)
-            exec(code)
+            #クエリごとのTop5表示
+            for column, candidate in enumerate(selectedTop5[]):
+                image = PIL.Image.open(candidate)
+                self.candidateImg.append(PIL.ImageTk.PhotoImage(image))
+                code = "self.candidate{} = Tk.Label(self, image=self.candidateImg[{}])".format(column, column)
+                exec(code)
+                code = "self.candidate{}.grid(row={}, column={}, padx = 10, pady = 10)".format(column, row, column)
+                exec(code)
 
 
 if __name__ == '__main__':
@@ -29,6 +37,26 @@ if __name__ == '__main__':
         '/Users/ryou/Documents/SourceTree/movie_summery/3.png',
         '/Users/ryou/Documents/SourceTree/movie_summery/4.png']
 
+    selectedTop5 = [
+        [
+            
+        ],
+        [
+
+        ]
+    ]
     f = Frame(filepath)
     f.pack(anchor = Tk.NW)
     f.mainloop()
+
+
+# epochごとの推論のログはここで読み込ませたいから、
+
+# <queryファイル名>
+# <top1ファイル名>
+# <top2ファイル名>
+# <top3ファイル名>
+# <top4ファイル名>
+# <top5ファイル名>
+
+# にしたい、jsonにすると処理と整形が面倒だからreadlineとかで読み込ませたいなぁ
